edaab4ef49f1f40a4970a36219d3b2d470ed3450
Anisul <anisul.huq90@gmail.com>
2013-12-19 22:24:11 -0500

    home controller

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Swag2::Application.config.secret_key_base = 'a54e85b9cccee6be489c75e82a7975326feb0afdb61d8c29cd44166b02d74fb0d8741cf10e0143cc6b75514ab8f27653872c97ba8b37923a545cb15733731ddb'
